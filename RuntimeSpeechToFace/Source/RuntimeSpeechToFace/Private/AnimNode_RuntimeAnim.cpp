74d0f33b3e66f3e53dd08c2f35f1c3cea86a5e47
weiyi <weiyiboynextdoor@163.com>
2026-01-30 22:51:30 +0800

    speech to anim curve

--- RuntimeSpeechToFace/Source/RuntimeSpeechToFace/Private/AnimNode_RuntimeAnim.cpp
+++ RuntimeSpeechToFace/Source/RuntimeSpeechToFace/Private/AnimNode_RuntimeAnim.cpp
@@ -1,1 +1,31 @@
 #include "AnimNode_RuntimeAnim.h"
+#include "Animation/AnimCurveUtils.h"
+
+void FAnimNode_RuntimeAnim::Update_AnyThread(const FAnimationUpdateContext& Context)
+{
+    Super::Update_AnyThread(Context);
+    GetEvaluateGraphExposedInputs().Execute(Context);
+    DeltaTime = Context.GetDeltaTime();
+}
+
+void FAnimNode_RuntimeAnim::Evaluate_AnyThread(FPoseContext& Output)
+{
+    if (RuntimeAnimation)
+    {
+        float CurTime = RuntimeAnimation->CurTime;
+        if (CurTime >= RuntimeAnimation->Duration)
+        {
+            return;
+        }
+        TMap<FName, float> CurveMap;
+        CurveMap.Reserve(RuntimeAnimation->FloatCurves.Num());
+        for (const FFloatCurve& FloatCurve : RuntimeAnimation->FloatCurves)
+        {
+            CurveMap.FindOrAdd(FloatCurve.GetName()) = FloatCurve.Evaluate(CurTime);
+        }
+        FBlendedCurve Curve;
+        UE::Anim::FCurveUtils::BuildUnsorted(Curve, CurveMap);
+        Output.Curve.Combine(Curve);
+        RuntimeAnimation->CurTime += DeltaTime;
+    }
+}
--- RuntimeSpeechToFace/Source/RuntimeSpeechToFace/Public/AnimNode_RuntimeAnim.h
+++ RuntimeSpeechToFace/Source/RuntimeSpeechToFace/Public/AnimNode_RuntimeAnim.h
@@ -5,6 +5,8 @@
 
 #include "AnimNode_RuntimeAnim.generated.h"
 
+#define UE_API RUNTIMESPEECHTOFACE_API
+
 USTRUCT(BlueprintInternalUseOnly)
 struct FAnimNode_RuntimeAnim : public FAnimNode_Base
 {
@@ -13,4 +15,12 @@ struct FAnimNode_RuntimeAnim : public FAnimNode_Base
 public:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = settings, meta = (AlwaysAsPin))
     TObjectPtr<class URuntimeAnimation> RuntimeAnimation;
+
+    UE_API void Update_AnyThread(const FAnimationUpdateContext& Context) override;
+
+    UE_API void Evaluate_AnyThread(FPoseContext& Output) override;
+
+    float DeltaTime = 0.0f;
 };
+
+#undef UE_API
